8cc87855cdd25ed83cd523ecaf71c7bd6b95dced
Jason Gorman <jason.gorman@codemanship.com>
2020-08-24 09:06:36 +0100

    Initial commit

---
+++ CdWarehouse.Test/DummyTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CdWarehouse.Test
+{
+
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void TestThatPasses()
+        {
+            
+        }
+        
+    }
+}
