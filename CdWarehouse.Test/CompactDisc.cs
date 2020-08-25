84d2f4c787d32684cb70c84aadf2ee39b4fd002b
Jason Gorman <jason.gorman@codemanship.com>
2020-08-25 08:42:18 +0100

    Added tests for Buy CD

---
+++ CdWarehouse.Test/CompactDisc.cs
@@ -0,0 +1,28 @@
+﻿namespace CdWarehouse.Test
+{
+    public class CompactDisc
+    {
+        private readonly IPayments _payments;
+
+        public CompactDisc(double price, int stock, IPayments payments)
+        {
+            _payments = payments;
+            Price = price;
+            Stock = stock;
+        }
+
+        private double Price { get; }
+        public double Stock { get; private set; }
+
+        public void Buy(int quantity, CreditCard card)
+        {
+            if(quantity > Stock)
+                throw new OutOfStockException();
+            
+            if(!_payments.IsAccepted(card, Price))
+                throw new PaymentRejectedException();
+            
+            Stock -= quantity;
+        }
+    }
+}
---
+++ CdWarehouse.Test/IPayments.cs
@@ -0,0 +1,7 @@
+﻿namespace CdWarehouse.Test
+{
+    public interface IPayments
+    {
+        bool IsAccepted(CreditCard card, in double price);
+    }
+}
